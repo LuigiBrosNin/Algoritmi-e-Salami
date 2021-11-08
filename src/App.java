a3c40df2a6c3d2887fe57aa5911634ed33b07fcd
LuigiBrosNin <emanuele109@hotmail.it>
2021-11-08 19:00:59 +0100

    starting point
    aggiunto il materiale mnkgame e lista di cose da fare
    non è molto ma è lavoro onesto, organizzazione :3

--- src/App.java
+++ src/App.java
@@ -1,3 +1,22 @@
+import mnkgame.*;
+// strategia
+/* 
+
+conviene lavorare in testplayer per la 
+
+1 - prima di tutto si controlla che non ci siano mosse vincenti per noi
+2 - poi si controllano le mosse vincenti avversarie (marca la prima casella trovata in quest'ordine dei casi)
+3 - lanci alpha beta pruning, ma ottimizzato con alcune cose
+    a - depth limitata (dinamica, basata su grandezza scacchiera)
+    b - timeout se ci mette troppo gioca a caso (ricorsivo, passa timeout -1 per 1 secondo di scarto per chiudere tutte le ricorsioni)
+    c - range limitato di caselle da controllare (giocando ottimizzato si gioca sempre su caselle adiacenti quindi stonks)
+    d - per il punto c trovare altri modi maybe per ottimizzare scegliere il migliore se non aumenta la complessità
+
+    <3 betto <3
+
+*/
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
